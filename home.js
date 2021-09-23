0fa98e27b8eadaaa4c2dd9a7093f59474dc353de
Harun AR <rasyid.id3@gmail.com>
2021-09-23 19:38:57 +0700

    editing home js in branch master

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
 // File untuk diupload pertama kali ke github di dalam master
 
-// Baris baru untuk commit di branch master
+// Baris baru untuk commit kedua di branch master
+
+// Baris baru untuk commit kedua di branch master
